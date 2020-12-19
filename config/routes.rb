92a271eb417c81c84a4f64cd6fe833b5597be9c1
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-19 11:22:42 +0100

    Registrations Controlled created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'pages/home'
-
-      resources :sessions, only: %i[create]
+      resources :sessions,      only: %i[create]
+      resources :registrations, only: %i[create]
     end
   end
   # root to: 'pages#home'

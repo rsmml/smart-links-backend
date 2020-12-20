5c5efa22a24111d318b26ee541d6249e1a0c4d7e
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-20 13:31:17 +0100

    Validations added to LanguageRule & SmartLink models |

--- app/models/language_rule.rb
+++ app/models/language_rule.rb
@@ -1,3 +1,8 @@
 class LanguageRule < ApplicationRecord
   belongs_to :smart_link
+
+  LANGUAGES = %w[ES DE EN]
+
+  validates :name, presence: true, inclusion: { in: LANGUAGES }
+  validates :url, presence: true
 end
--- app/models/smart_link.rb
+++ app/models/smart_link.rb
@@ -1,4 +1,9 @@
 class SmartLink < ApplicationRecord
   belongs_to :user
   has_many :language_rules
+
+  LANGUAGES = %w[ES DE EN]
+
+  validates :name, presence: true, inclusion: { in: LANGUAGES }
+  validates :url, presence: true
 end
